a46c73504d23fb3128e8effca5694150b92494d2
ColinWeiss <1805094167@qq.com>
2023-09-05 14:29:03 +0800

    -

--- Assets/FontResource.cs
+++ Assets/FontResource.cs
@@ -1,4 +1,5 @@
 ﻿using FontStashSharp;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Colin.Core.Assets
 {
@@ -8,17 +9,28 @@ namespace Colin.Core.Assets
 
         public static FontSystem GlowSans { get; private set; }
 
-        public string Name { get; }
+        public string Name => "字体资源";
 
         public float Progress { get; set; }
 
+        private static Dictionary<string, FontSystem> _fonts = new Dictionary<string, FontSystem>( );
+        public static Dictionary<string, FontSystem> Fonts => _fonts;
+
         public void LoadResource( )
         {
-            Unifont = new FontSystem( );
-            Unifont.AddFont( Colin.Assets.Assets.Unifont );
-
-            GlowSans = new FontSystem( );
-            GlowSans.AddFont( Colin.Assets.Assets.GlowSans );
+            if(!Directory.Exists( string.Concat( EngineInfo.Engine.Content.RootDirectory, "/Fonts" ) ))
+                return;
+            FontSystem _font;
+            string _fileName;
+            string[ ] _fontFileNames = Directory.GetFiles( string.Concat( EngineInfo.Engine.Content.RootDirectory, "/Textures" ), "*.xnb*", SearchOption.AllDirectories );
+            for(int count = 0 ; count < _fontFileNames.Length ; count++)
+            {
+                Progress = count / _fontFileNames.Length + 1 / _fontFileNames.Length;
+                _font = new FontSystem( );
+                _font.AddFont( File.ReadAllBytes( _fontFileNames[count] ));
+                _fileName = IGameResource.ArrangementPath( _fontFileNames[count] );
+                _fonts.Add( _fileName , _font );
+            }
         }
     }
 }
