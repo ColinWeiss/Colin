f0a6e16b4d6f14d33c73529df5d96a435e7acb26
ColinWeiss <1805094167@qq.com>
2023-09-08 00:52:03 +0800

    -

---
+++ Extensions/StringExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Colin.Core.Extensions
+{
+    public static class StringExt
+    {
+        public static int GetMsnHashCode( this string str )
+        {
+            int result = 0;
+            int len = str.Length;
+            for(int i = 0 ; i < len ; i++)
+                result += ( result + str[i] * 3737 %1000000007)% 1000000007;
+            return result;
+        }
+    }
+}
