bbd6cd6c416416467f33e450b1ffa6c202b95a1d
ColinWeiss <1805094167@qq.com>
2024-01-11 22:39:16 +0800

    -

--- Localizations/Chinese.cs
+++ Localizations/Chinese.cs
@@ -4,8 +4,14 @@
     {
         public override void SetDefaultTexts()
         {
-            Texts.Add("ON", "开");
-            Texts.Add("OFF", "关");
+            Texts.Add("Normal.Interactive.ON", "开");
+            Texts.Add("Normal.Interactive.OFF", "关");
+            Texts.Add("Normal.Interactive.Back", "返回");
+            Texts.Add("Normal.Interactive.Exit", "退出");
+            Texts.Add("Normal.Interactive.Load", "加载");
+            Texts.Add("Normal.Interactive.Save", "保存");
+            Texts.Add("Normal.Interactive.Delete", "删除");
+
             base.SetDefaultTexts();
         }
     }
