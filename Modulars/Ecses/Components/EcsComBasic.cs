66d331b6d305822697370c914973608de83a833c
ColinWeiss <1805094167@qq.com>
2024-01-20 22:47:28 +0800

    -

---
+++ Modulars/Ecses/Components/EcsComBasic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colin.Core.Modulars.Ecses.Components
+{
+    public class EcsComBasic
+    {
+        public float TimeScale = 1f;
+
+
+    }
+}
