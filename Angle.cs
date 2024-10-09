4be59df68f6a43b75fe397a5f6ba0f30a1f621cb
ColinWeiss <1805094167@qq.com>
2024-10-09 23:25:50 +0800

    - 文件至MD5; 角代码.

---
+++ Angle.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+
+namespace Colin.Core
+{
+  public struct Angle : IEquatable<Angle>
+  {
+    public static readonly Angle Up = new Angle(270);
+    public static readonly Angle Down = new Angle(90);
+    public static readonly Angle Left = new Angle(180);
+    public static readonly Angle Right = new Angle(0);
+
+    public double Degrees
+    {
+      get => _degrees;
+      set
+      {
+        _degrees = value;
+        Normalize();
+      }
+    }
+
+    public double Radians
+    {
+      get => (_degrees / 360.0) * Math.PI * 2.0;
+      set
+      {
+        Degrees = (value / (Math.PI * 2.0)) * 360.0;
+      }
+    }
+
+    public float DegreesF => (float)Degrees;
+    public float RadiansF => (float)Radians;
+
+    private double _degrees;
+
+    public Angle(double degrees)
+    {
+      _degrees = degrees;
+      Normalize();
+    }
+
+    public Angle(Vector2 vector)
+    {
+      _degrees = (Math.Atan2(vector.Y, vector.X) * 360.0) / (Math.PI * 2.0);
+      Normalize();
+    }
+
+    public Angle(Vector2 point1, Vector2 point2) : this(point2 - point1) { }
+
+    public static Angle FromRadians(double radians)
+    {
+      var angle = new Angle();
+      angle.Radians = radians;
+      return angle;
+    }
+
+    public double Difference(Angle other) =>
+        ((Degrees - other.Degrees + 180.0) % 360.0 + 360.0) % 360.0 - 180.0;
+
+    public Vector2 ToVector2() =>
+        new Vector2((float)Math.Cos(Radians), (float)Math.Sin(Radians));
+
+    public static Angle Lerp(Angle angle1, Angle angle2, double value)
+    {
+      var diff = angle1.Difference(angle2);
+      return angle1 - diff * value;
+    }
+
+    public static double ToDegrees(double radians) =>
+        (radians / (Math.PI * 2.0)) * 360.0;
+
+    public static double ToRadians(double degrees) =>
+        (degrees / 360.0) * Math.PI * 2.0;
+
+    public bool Equals(Angle other) =>
+        _degrees == other._degrees;
+
+    public override bool Equals(object obj)
+    {
+      if (obj is Angle)
+      {
+        return Equals(this);
+      }
+      return false;
+    }
+
+    public static Angle operator +(Angle a1, Angle a2) =>
+        new Angle(a1._degrees + a2._degrees);
+
+    public static Angle operator +(Angle a, double num) =>
+        new Angle(a._degrees + num);
+
+    public static Angle operator +(Angle a, float num) =>
+        new Angle(a._degrees + num);
+
+    public static Angle operator +(Angle a, int num) =>
+        new Angle(a._degrees + num);
+
+    public static Angle operator +(double num, Angle a) =>
+        new Angle(a._degrees + num);
+
+    public static Angle operator +(float num, Angle a) =>
+        new Angle(a._degrees + num);
+
+    public static Angle operator +(int num, Angle a) =>
+        new Angle(a._degrees + num);
+
+    public static Angle operator -(Angle a1, Angle a2) =>
+        new Angle(a1._degrees - a2._degrees);
+
+    public static Angle operator -(Angle a, double num) =>
+        new Angle(a._degrees - num);
+
+    public static Angle operator -(Angle a, float num) =>
+        new Angle(a._degrees - num);
+
+    public static Angle operator -(Angle a, int num) =>
+        new Angle(a._degrees - num);
+
+    public static Angle operator -(double num, Angle a) =>
+        new Angle(num - a._degrees);
+
+    public static Angle operator -(float num, Angle a) =>
+        new Angle(num - a._degrees);
+
+    public static Angle operator -(int num, Angle a) =>
+        new Angle(num - a._degrees);
+
+    public static Angle operator *(Angle a, double num) =>
+        new Angle(a._degrees * num);
+
+    public static Angle operator *(Angle a, float num) =>
+        new Angle(a._degrees * num);
+
+    public static Angle operator *(Angle a, int num) =>
+        new Angle(a._degrees * num);
+
+    public static Angle operator *(double num, Angle a) =>
+        new Angle(a._degrees * num);
+
+    public static Angle operator *(float num, Angle a) =>
+        new Angle(a._degrees * num);
+
+    public static Angle operator *(int num, Angle a) =>
+        new Angle(a._degrees * num);
+
+    public static Angle operator /(Angle a, double num) =>
+        new Angle(a._degrees / num);
+
+    public static Angle operator /(Angle a, float num) =>
+        new Angle(a._degrees / num);
+
+    public static Angle operator /(Angle a, int num) =>
+        new Angle(a._degrees / num);
+
+    public static Angle operator /(double num, Angle a) =>
+        new Angle(num / a._degrees);
+
+    public static Angle operator /(float num, Angle a) =>
+        new Angle(num / a._degrees);
+
+    public static Angle operator /(int num, Angle a) =>
+        new Angle(num / a._degrees);
+
+    public static bool operator >(Angle a1, Angle a2) =>
+        a1._degrees > a2._degrees;
+
+    public static bool operator <(Angle a1, Angle a2) =>
+        a1._degrees < a2._degrees;
+
+    public static bool operator >=(Angle a1, Angle a2) =>
+        a1._degrees >= a2._degrees;
+
+    public static bool operator <=(Angle a1, Angle a2) =>
+        a1._degrees <= a2._degrees;
+
+    public static bool operator ==(Angle a1, Angle a2) =>
+        a1._degrees == a2._degrees;
+
+    public static bool operator !=(Angle a1, Angle a2) =>
+        a1._degrees != a2._degrees;
+
+    public static implicit operator Angle(Vector2 value) => new Angle(value);
+    public static implicit operator Angle((float x, float y) value) => new Angle(new Vector2(value.x, value.y));
+    public static implicit operator Angle(double degrees) => new Angle(degrees);
+
+    public static explicit operator Vector2(Angle angle) => angle.ToVector2();
+
+    public override int GetHashCode() =>
+        _degrees.GetHashCode();
+
+    public override string ToString() =>
+        _degrees.ToString();
+
+    void Normalize() =>
+        _degrees = (_degrees % 360.0 + 360.0) % 360.0;
+  }
+}
---
+++ IO/FileCheck/FileToMD5.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colin.Core.IO.FileCheck
+{
+  /// <summary>
+  /// 提供对文件进行 MD5 操作的方法.
+  /// </summary>
+  public class FileToMD5
+  {
+    private static MD5 _md5;
+    public static MD5 MD5 => _md5 ??= MD5.Create();
+
+    public static void Load()
+    {
+      List<string> paths = new List<string>();
+      bool flag = true; ;
+      bool[] results = CheckFiles(paths).ToArray();
+      for (int count = 0; count < results.Length; count++)
+      {
+        flag = results[count];
+        if (flag is false)
+        {
+          //TODO: 抛出, 并予以备选流.
+        }
+      }
+    }
+
+    public static IEnumerable<bool> CheckFiles(List<string> filePaths)
+    {
+      string path;
+      string md5;
+      bool result;
+      for (int count = 0; count < filePaths.Count; count++)
+      {
+        path = filePaths[count];
+        md5 = GetFileMd5Hash(path);
+        result = true;//TODO: 校验
+        yield return result;
+      }
+    }
+
+    public static string GetFileMd5Hash(string filePath)
+    {
+      using (Task<byte[]> stream = File.ReadAllBytesAsync(filePath))
+      {
+        MD5 md5 = MD5.Create();
+        byte[] hashValue = md5.ComputeHash(stream.Result);
+        StringBuilder hex = new StringBuilder(hashValue.Length * 2);
+        foreach (byte b in hashValue)
+        {
+          hex.Append(b);
+        }
+        return hex.ToString();
+      }
+    }
+  }
+}
