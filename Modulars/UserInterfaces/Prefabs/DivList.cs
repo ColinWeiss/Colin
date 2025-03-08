04779e37d11eed451d75c9b8ced6c30037df3f0e
ColinWeiss <1805094167@QQ.COM>
2025-03-09 03:10:40 +0800

    ※ 新增DivList.

---
+++ Modulars/UserInterfaces/Prefabs/DivList.cs
@@ -0,0 +1,37 @@
+﻿using Colin.Core.Modulars.UserInterfaces.Controllers;
+using Colin.Core.Modulars.UserInterfaces.Renderers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colin.Core.Modulars.UserInterfaces.Prefabs
+{
+  public class DivList : Div
+  {
+    public DivList(string name, bool isCanvas = false) : base(name, isCanvas)
+    {
+      List = new Div("List");
+    }
+    public Div List;
+    public override void DivInit()
+    {
+      Layout.ScissorEnable = true;
+      Layout.ScissorWidth = (int)Layout.Width;
+      Layout.ScissorHeight = (int)Layout.Height;
+
+      var linear = List.BindController<LinearMenuController>();
+      linear.AutoSetSize = true;
+      linear.DivInterval = 4;
+      base.Register(List);
+      base.DivInit();
+    }
+    public void GetSlider(Slider slider)
+    {
+      slider.Bind(List, this);
+    }
+    public override bool Register(Div div, bool doInit = false)
+    {
+      return List.Register(div, doInit);
+    }
+  }
+}
