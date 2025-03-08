f45b05207f80d4062f35bf2111d054e790570959
ColinWeiss <1805094167@QQ.COM>
2025-03-09 03:59:39 +0800

    ※ 修改DebugFramework初步.

---
+++ Common/SceneRenderModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Colin.Core.Common
+{
+  public class SceneRenderModule : SceneModule, IRenderableISceneModule
+  {
+    public RenderTarget2D RawRt { get; set; }
+    public bool RawRtVisible { get; set; }
+    public bool Presentation { get; set; }
+
+    public virtual void DoRawRender(GraphicsDevice device, SpriteBatch batch) { }
+
+    public virtual void DoRegenerateRender(GraphicsDevice device, SpriteBatch batch) { }
+  }
+}
