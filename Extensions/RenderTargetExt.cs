e469f150e52f952dadc243dad64304807f5a810e
MoebiusMeow <MoebiusMeow@qq.com>
2025-05-01 19:45:43 +0800

    update RenderTargetExt

--- Extensions/RenderTargetExt.cs
+++ Extensions/RenderTargetExt.cs
@@ -28,6 +28,19 @@
       RenderTargetUsage.PreserveContents);
       return renderTarget;
     }
+    public static RenderTarget2D CreateHDR(int width, int height)
+    {
+      RenderTarget2D renderTarget = new RenderTarget2D(
+      CoreInfo.Graphics.GraphicsDevice,
+      width,
+      height,
+      false,
+      SurfaceFormat.Vector4,
+      DepthFormat.None,
+      0,
+      RenderTargetUsage.PreserveContents);
+      return renderTarget;
+    }
     public static RenderTarget2D CreateWithDepth(int width, int height)
     {
       RenderTarget2D renderTarget = new RenderTarget2D(
