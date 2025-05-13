9ff545293ba21dbd46e3338c86e014279d8a92ff
ColinWeiss <1805094167@QQ.COM>
2025-05-13 21:41:38 +0800

    ※ 导入AssimpNetter库.

---
+++ Graphics/Models/GlbModel.cs
@@ -0,0 +1,44 @@
+﻿using Assimp;
+using Assimp.Configs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AssimpModel = Assimp.Scene;
+
+namespace Colin.Core.Graphics.Models
+{
+  public class GlbModel
+  {
+    private AssimpModel _assimpModel;
+
+    public void OuputForeach()
+    {
+      Console.WriteLine("输出模型 Node:");
+      ForeachOutput(_assimpModel.RootNode, 0);
+    }
+    private void ForeachOutput(Assimp.Node node, int depth)
+    {
+      Assimp.Node child;
+      string n = "";
+      for (int i = 0; i < depth; i++)
+      {
+        n += "﹂";
+      }
+      for (int i = 0; i < node.ChildCount; i++)
+      {
+        child = node.Children[i];
+        Console.WriteLine(n + child.Name);
+        ForeachOutput(child, depth++);
+      }
+    }
+
+    public static GlbModel FormFile(string path)
+    {
+      var importer = new Assimp.AssimpContext();
+      importer.SetConfig(new GlobalScaleConfig(1f));
+      GlbModel model = new GlbModel();
+      model._assimpModel = importer.ImportFile(path);
+      return model;
+    }
+  }
+}
