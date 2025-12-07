b0f8dd99d9b9812622f2cd231d11ac86754d7bb9
ColinWeiss <1805094167@QQ.COM>
2025-12-08 01:19:16 +0800

    [251208] Use AssimpNetter

--- Graphics/Models/GlbModel.cs
+++ Graphics/Models/GlbModel.cs
@@ -1,16 +1,41 @@
-﻿using Assimp.Configs;
+﻿using Assimp;
+using Assimp.Configs;
+using Microsoft.Xna.Framework.Graphics;
+using System.Drawing;
 using AssimpModel = Assimp.Scene;
 
 namespace Colin.Core.Graphics.Models
 {
   public class GlbModel
   {
-    private AssimpModel _assimpModel;
+    private AssimpModel _model;
 
     public void OuputForeach()
     {
-      Console.WriteLine("输出模型 Node:");
-      ForeachOutput(_assimpModel.RootNode, 0);
+      ForeachOutput(_model.RootNode, 0);
+      MaterialProperty[] mats;
+      Console.WriteLine(_model.Textures.Count);
+      Texture2D t;
+      using (var stream = new MemoryStream(_model.Textures[0].CompressedData))
+      {
+        t = Texture2D.FromStream(CoreInfo.Graphics.GraphicsDevice, stream);
+      }
+      using (FileStream fs = new FileStream("test.png", FileMode.Create))
+      {
+        t.SaveAsPng(fs, t.Width, t.Height);
+      }
+
+      for (int i = 0; i < _model.Materials.Count; i++)
+      {
+        mats = _model.Materials[i].GetAllProperties();
+        for (int j = 0; j < mats.Length; j++)
+        {
+          Console.WriteLine(mats[j].FullyQualifiedName + " " + mats[j].RawData.Length);
+          if (mats[j].Name == "$mat.gltf.pbrMetallicRoughness.baseColorFactor")
+          {
+          }
+        }
+      }
     }
     private void ForeachOutput(Assimp.Node node, int depth)
     {
@@ -33,7 +58,11 @@ namespace Colin.Core.Graphics.Models
       var importer = new Assimp.AssimpContext();
       importer.SetConfig(new GlobalScaleConfig(1f));
       GlbModel model = new GlbModel();
-      model._assimpModel = importer.ImportFile(path);
+      model._model = importer.ImportFile(path,
+        PostProcessSteps.Triangulate |
+        PostProcessSteps.GenerateNormals |
+        PostProcessSteps.FlipUVs |
+        PostProcessSteps.MakeLeftHanded);
       return model;
     }
   }
