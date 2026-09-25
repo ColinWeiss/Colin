3338fc5f847b4e6a6043167f3755bf731c433cd4
ColinWeiss <1805094167@QQ.COM>
2026-09-25 19:22:17 +0800

    [260925]

--- Usings.cs
+++ Usings.cs
@@ -24,6 +24,7 @@ global using Console = Colin.Core.Console;
 global using Keys = Microsoft.Xna.Framework.Input.Keys;
 global using Rectangle = Microsoft.Xna.Framework.Rectangle;
 global using RectangleF = Colin.Core.RectangleF;
+global using Point = Microsoft.Xna.Framework.Point;
 global using Vector2 = Microsoft.Xna.Framework.Vector2;
 global using Vector3 = Microsoft.Xna.Framework.Vector3;
 global using Vector4 = Microsoft.Xna.Framework.Vector4;
